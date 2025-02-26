2cc5a9abe7fb989c6abeee0e370b0ea6f396a822
Emanuelly <emanuellydssenai@gmail.com>
2025-02-26 08:05:17 -0300

    inicio do repositório I3DS-2025

---
+++ 02-react-vite/src/Ola.jsx
@@ -0,0 +1,7 @@
+const Ola = () => {
+    return (
+        <h1>Olá, mundo!</h1>
+    )
+}
+
+export default Ola
---
+++ 03-devlinks/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000, //Porta para conexão, altere quando necessário
+    open: true, //Abre automáticamente o navegador e inicia o server
+    host: true, //Habilitar redes externas
+
+  },
+});
